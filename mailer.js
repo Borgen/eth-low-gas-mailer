df56e785ae212971da60149b294c911882df2585
Borgen <okan.konur@gmail.com>
2022-03-29 20:26:36 +0300

    parse int

--- mailer.js
+++ mailer.js
@@ -6,60 +6,52 @@ const Fs = require("fs");
 
 var toList = process.env.TOLIST;
 
-if(toList){    
-    console.log("Mail gönderilecekler listesi: " + toList);
-}
-else{
-    toList = "***REMOVED***";    
-    console.log("Mail gönderilecekler listesi: " + toList);
+if (toList) {
+  console.log("Mail gönderilecekler listesi: " + toList);
+} else {
+  toList = "***REMOVED***";
+  console.log("Mail gönderilecekler listesi: " + toList);
 }
 
 module.exports = {
 
-    send: async function send(avgGasPrice, lastBlockNumber) {
+  send : async function send(avgGasPrice, lastBlockNumber) {
+    Fs.readFile("./email.html", "utf-8", (error, htmlContent) => {
+      if (error) {
+        console.log("E-Posta dosyası okunurken hata alındı!", error);
+        return;
+      }
 
-        Fs.readFile("./email.html", "utf-8", (error, htmlContent) => {
-               
-                if(error){
-                    console.log("E-Posta dosyası okunurken hata alındı!", error);
-                    return;
-                }
-
-                let template = Handlebars.compile(htmlContent);
-                let replacements = {
-                     gasPrice: avgGasPrice,
-                     lastBlockNumber: lastBlockNumber
-                };
-                let htmlResultContent = template(replacements);
-                sendMail(htmlResultContent);
-
-            }); 
-
-        }
+      let template = Handlebars.compile(htmlContent);
+      let replacements = {
+        gasPrice : avgGasPrice,
+        lastBlockNumber : lastBlockNumber
+      };
+      let htmlResultContent = template(replacements);
+      sendMail(htmlResultContent);
+    });
+  }
 
 };
 
-async function sendMail(htmlResultContent){
-        
-    let transporter = Nodemailer.createTransport({
-                    host: "smtp.gmail.com",
-                    port: 587,
-                    secure: false, // true for 465, false for other ports
-                    auth: {
-                        user: "***REMOVED***", 
-                        pass: "***REMOVED***"
-                    },
-                });
+async function sendMail(htmlResultContent) {
 
-                // send mail with defined transport object
-                let info = await transporter.sendMail({
-                    from: '"Eth Pompacısı" <***REMOVED***>', // sender address
-                    to: toList, // list of receivers
-                    subject: "Ortalama Eth Gaz Fiyatı Düşük! 💲✂️,💰", // Subject line
-                    html: htmlResultContent 
-                });
+  let transporter = Nodemailer.createTransport({
+    host : "smtp.gmail.com",
+    port : 587,
+    secure : false, // true for 465, false for other ports
+    auth : {user : "***REMOVED***", pass : "***REMOVED***"},
+  });
 
-                console.log("Message sent: %s", info.messageId);
-                // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from : '"Eth Pompacısı" <***REMOVED***>', // sender address
+    to : toList,                                    // list of receivers
+    subject :
+        "Ortalama Eth Gaz Fiyatı Düşük! 💲✂️💰", // Subject line
+    html : htmlResultContent
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 }
-               
--- server.js
+++ server.js
@@ -2,92 +2,89 @@ var CronJob = require('cron').CronJob
 var Mailer = require('./mailer')
 var Web3 = require('web3');
 
-
-
-
 var provider;
 var web3Provider;
 var web3Client;
 
 var everyNthHour = process.env.PERIOD;
-var avgGasPriceLimit = 30;
+var avgGasPriceLimit = 130;
 
 if (isNumeric(process.env.PRICELIMIT)) {
-    console.log("Gas limit bilgisi okundu: " + process.env.PRICELIMIT);
-    avgGasPriceLimit = process.env.PRICELIMIT;
+  console.log("Gas limit bilgisi okundu: " + process.env.PRICELIMIT);
+  avgGasPriceLimit = process.env.PRICELIMIT;
 }
-
 
 checkGasPrice();
 
-
-//start job for every Nth hour
+// start job for every Nth hour
 
 if (isNumeric(everyNthHour)) {
 
-    try {
-        console.log("Periyod bilgisi okundu. " + everyNthHour + " saatte bir olarak alınıyor.");
-        var job = new CronJob('0 0 */' + everyNthHour + ' * * *', checkGasPrice, null, false, 'Europe/Istanbul');
+  try {
+    console.log("Periyod bilgisi okundu. " + everyNthHour +
+                " saatte bir olarak alınıyor.");
+    var job = new CronJob('0 0 */' + everyNthHour + ' * * *', checkGasPrice,
+                          null, false, 'Europe/Istanbul');
 
-        job.start();
-        console.log("job başlatıldı.");
+    job.start();
+    console.log("job başlatıldı.");
 
-    } catch (e) {
-        console.log("CronJob setlenirken hata", e);
-    }
+  } catch (e) {
+    console.log("CronJob setlenirken hata", e);
+  }
+} else {
+
+  try {
+
+    console.log("Periyod bilgisi okunmadı, 30 saniyede bir olarak alınıyor.");
+    var job = new CronJob('*/30 * * * * *', checkGasPrice, null, false,
+                          'Europe/Istanbul'); // test every 30th second
+
+    job.start();
+
+    console.log("job başlatıldı.");
+
+  } catch (e) {
+    console.log("CronJob setlenirken hata", e);
+  }
 }
-else {
-
-    try {
-
-        console.log("Periyod bilgisi okunmadı, 30 saniyede bir olarak alınıyor.");
-        var job = new CronJob('*/30 * * * * *', checkGasPrice, null, false, 'Europe/Istanbul'); //test every 30th second
-
-        job.start();
-
-        console.log("job başlatıldı.");
-
-    } catch (e) {
-        console.log("CronJob setlenirken hata", e);
-    }
-}
-
-
-
 
 function checkGasPrice() {
 
-    provider = 'https://mainnet.strongblock.com/34675c4172308743ec869ee6eec7c654679270fb';
-    web3Provider = new Web3.providers.HttpProvider(provider);
-    web3Client = new Web3(web3Provider);
-    console.log("Eth gas fiyati kontrol ediliyor...");
+  provider =
+      'https://mainnet.strongblock.com/34675c4172308743ec869ee6eec7c654679270fb';
+  web3Provider = new Web3.providers.HttpProvider(provider);
+  web3Client = new Web3(web3Provider);
+  console.log("Eth gas fiyati kontrol ediliyor...");
 
-    
-    let avgGasPricePromise = web3Client.eth.getGasPrice();
-    let lastBlockNumberPromise = web3Client.eth.getBlockNumber();
+  let avgGasPricePromise = web3Client.eth.getGasPrice();
+  let lastBlockNumberPromise = web3Client.eth.getBlockNumber();
 
-    Promise.all([avgGasPricePromise, lastBlockNumberPromise]).then((values) => { sendMail(values[0], values[1]);  });
-
-
+  Promise.all([ avgGasPricePromise, lastBlockNumberPromise ])
+      .then((values) => { sendMail(values[0], values[1]); });
 }
 
 function sendMail(avgGasPrice, lastBlockNumber) {
 
-    let avgGasPriceGwei = web3Client.utils.fromWei(avgGasPrice, 'gwei');
-    console.log("Ortalama fiyat: " + avgGasPriceGwei + " gwei");
-    console.log("Son blok: " + lastBlockNumber);
-    
-    if (avgGasPriceGwei < avgGasPriceLimit) {
-        console.log("Ortlama fiyat limit olan " + avgGasPriceLimit + " gwei'den düşük! Mail gönderiliyor...");
-        Mailer.send(avgGasPriceGwei, lastBlockNumber).catch(console.error);
-        return;
-    }
+  let avgGasPriceGwei = web3Client.utils.fromWei(avgGasPrice, 'gwei');
+  console.log("Ortalama fiyat: " + avgGasPriceGwei + " gwei");
+  console.log("Son blok: " + lastBlockNumber);
 
-    console.log("Ortalama fiyat düşük değil mail gönderilmeyecek. Başka sefere inş");
+  if (parseInt(avgGasPriceGwei, 10) < parseInt(avgGasPriceLimit, 10)) {
+    console.log("Ortlama fiyat limit olan " + avgGasPriceLimit +
+                " gwei'den düşük! Mail gönderiliyor...");
+    Mailer.send(avgGasPriceGwei, lastBlockNumber).catch(console.error);
+    return;
+  }
+
+  console.log(
+      "Ortalama fiyat düşük değil mail gönderilmeyecek. Başka sefere inş");
 }
 
 function isNumeric(str) {
-    if (typeof str != "string") return false // we only process strings!  
-    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
-        !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+  if (typeof str != "string")
+    return false          // we only process strings!
+    return !isNaN(str) && // use type coercion to parse the _entirety_ of the
+                          // string (`parseFloat` alone does not do this)...
+           !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
 }
